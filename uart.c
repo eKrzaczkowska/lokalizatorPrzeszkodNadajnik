eda566ecc5823b2aa8085353731b5271aec42c0c
eKrzaczkowska <112576532+eKrzaczkowska@users.noreply.github.com>
2023-01-11 15:27:30 +0100

    Create uart.c

---
+++ uart.c
@@ -0,0 +1,29 @@
+/*****************USART*************/
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "uart.h"
+
+void USART_Init()
+{
+	UBRRH = (MYUBRR>>8);    	// Set baud rate
+	UBRRL = MYUBRR;
+	UCSRC = (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);	// 8bit data format
+	UCSRB = (1<<TXEN) | (1<<RXEN); 	
+
+}
+
+void USARTWriteChar(char data) { // blocking
+	while(!(UCSRA & (1<<UDRE))) {}
+	UDR=data;
+}
+
+void USART_TransmitString(char *data)
+{
+    while (*data) USARTWriteChar(*data++);
+}
+
+
